22c11dc12f7a3ea48b735d263dd685dabe481691
Riyanshu Patro <riyanshu@push.org>
2025-08-19 16:08:23 +0530

    chore: add custom rpc url for the solana in simulate and ballsy

--- tutorials/universal-counter/app/src/main.tsx
+++ tutorials/universal-counter/app/src/main.tsx
@@ -22,6 +22,12 @@ import './index.css'
 // Define Wallet Config
 const walletConfig = {
   network: PushUI.CONSTANTS.PUSH_NETWORK.TESTNET,
+  chainConfig: {
+    rpcUrls: {
+      'eip155:11155111': ['https://sepolia.gateway.tenderly.co/'],
+      'solana:EtWTRABZaYq6iMfeYKouRu166VU2xqa1': ['https://weathered-empty-rain.solana-devnet.quiknode.pro/278a5c4fa65bc6656ff0ff65ab2c3d1004fd00f9/'],
+    },
+  },
 };
 
 createRoot(document.getElementById('root')!).render(
