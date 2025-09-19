b79c792822cf1069ffab9ac154960affe9a705ce
Riyanshu Patro <riyanshu@push.org>
2025-09-19 21:28:55 +0530

    fix: deploy ballsy to https://ballsy.push.org/

--- tutorials/universal-counter/ballsy-app/vite.config.js
+++ tutorials/universal-counter/ballsy-app/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/push-chain-ballsy-app-deployment',  // Use relative paths
+  base: '/',  // Use relative paths
   resolve: {
     alias: {
       process: 'process/browser',
