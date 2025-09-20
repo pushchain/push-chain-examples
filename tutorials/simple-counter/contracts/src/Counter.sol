bad21f5cacb16d5ae4d9736d0b7d444c2bed0bf1
harshrajat <harsh@epns.io>
2025-09-20 06:55:40 +0400

    chore: update Solidity version to use caret notation for all counter contracts

--- tutorials/simple-counter/contracts/src/Counter.sol
+++ tutorials/simple-counter/contracts/src/Counter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.22;
+pragma solidity ^0.8.22;
 
 contract Counter {
     uint256 public countPC;
--- tutorials/universal-counter/contracts/src/UniversalCounter.sol
+++ tutorials/universal-counter/contracts/src/UniversalCounter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.22;
+pragma solidity ^0.8.22;
 
 struct UniversalAccountId {
     // @notice Chain namespace identifier (e.g., "eip155" or "solana").
