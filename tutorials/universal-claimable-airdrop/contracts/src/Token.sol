099eede8f38eb19257ac2d51f9f9a90c4bad6a13
harshrajat <harsh@epns.io>
2026-01-16 17:33:01 +0400

    chore: replace token interface with full ERC20 implementation
    Replace IUnicornToken interface with complete Token contract implementation. Add constructor with initial supply minting (1M tokens), public mint function, and inherit from OpenZeppelin ERC20. Update import from IERC20 to ERC20 base contract.

--- tutorials/universal-claimable-airdrop/contracts/src/Token.sol
+++ tutorials/universal-claimable-airdrop/contracts/src/Token.sol
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// Interface for $UNICORN token with mint function
-interface IUnicornToken is IERC20 {
-    function mint(address to, uint256 amount) external;
+contract Token is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 1_000_000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
 }
